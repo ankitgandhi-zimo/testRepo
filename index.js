eef452345c963875ada2fc504d4b90e6619558d5
Zimo <ankit.zimo@outlook.com>
2023-12-06 14:46:16 +0530

    fetaure2

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 console.log("feature2 branch changes");
-<<<<<<< HEAD
 console.log("feature2 branch new changes");
-=======
 console.log("this is main branch changes");
-
->>>>>>> 2932198f1664b0345498972c8e485e3928986541
